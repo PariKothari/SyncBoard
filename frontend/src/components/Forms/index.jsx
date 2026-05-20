45d33f49ee950a1f8457fde7fe1a50f3208b00b4
PariKothari <parikothari9251@gmail.com>
2026-05-20 19:56:46 +0530

    added join room logic

--- frontend/src/components/Forms/index.jsx
+++ frontend/src/components/Forms/index.jsx
@@ -12,7 +12,7 @@ const Forms=({uuid,socket,setUser})=>{
             </div>
             <div className="col-md-4 mt-5 form-box p-5 border border-2 border-primary rounded-2 mx-auto d-flex flex-column align-items-center j">
                 <h1 className="text-primary fw-bold">Join Room</h1>
-                <JoinRoomForm/>
+                <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
             </div>        
         </div>
     );
